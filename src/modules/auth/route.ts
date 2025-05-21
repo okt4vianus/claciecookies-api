3c2066fb0e76ce36c086d3420e7d75b997204856
M Haidar Hanif <git@mhaidarhanif.com>
2025-05-21 15:11:02 +0700

    feat: login user with password

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -1,9 +1,9 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
-import { RegisterBodySchema, UsersSchema } from "./schema";
+import { LoginBodySchema, RegisterBodySchema, UsersSchema } from "./schema";
 import { prisma } from "../../lib/prisma";
 import { ErrorResponseSchema } from "../common/schema";
 import { UserSchema } from "../../generated/zod";
-import { hashPassword } from "../../lib/password";
+import { comparePassword, hashPassword } from "../../lib/password";
 
 export const authRoute = new OpenAPIHono();
 
@@ -47,7 +47,68 @@ authRoute.openapi(
 
       return c.json(newUser);
     } catch (error) {
-      return c.json({ message: "User register failed", error: error }, 400);
+      return c.json({ message: "User register failed", error: error }, 500);
+    }
+  }
+);
+
+// ✅ POST /auth/login
+authRoute.openapi(
+  createRoute({
+    tags,
+    summary: "Login user",
+    method: "post",
+    path: "/login",
+    request: {
+      body: {
+        content: { "application/json": { schema: LoginBodySchema } },
+      },
+    },
+    responses: {
+      201: {
+        content: { "application/json": { schema: UserSchema } },
+        description: "User logged in successfully",
+      },
+      400: {
+        description: "User login failed",
+      },
+      500: {
+        description: "User login failed",
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const body = c.req.valid("json");
+      const { password, ...userData } = body;
+
+      const user = await prisma.user.findUnique({
+        where: { email: userData.email },
+        include: { password: true },
+      });
+
+      if (!user) {
+        return c.json({ message: "User not found" }, 400);
+      }
+
+      if (!user.password) {
+        return c.json({ message: "User does not have a password" }, 400);
+      }
+
+      const isPasswordValid = await comparePassword(
+        password,
+        user.password.hash
+      );
+
+      if (!isPasswordValid) {
+        return c.json({ message: "Invalid password" }, 400);
+      }
+
+      const { password: _, ...userWithoutPassword } = user;
+
+      return c.json(userWithoutPassword);
+    } catch (error) {
+      return c.json({ message: "User login failed", error: error }, 500);
     }
   }
 );
