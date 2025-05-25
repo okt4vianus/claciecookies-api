fa84b8ac117dd7d4d9e764111bb933d2c573c178
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-05-25 23:12:28 +0800

    feat: login user with password

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -1,9 +1,9 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { prisma } from "../../lib/prisma";
 import { ErrorResponseSchema, SuccessResponseSchema } from "../common/schema";
-import { RegisterBodySchema } from "./schema";
+import { LoginBodySchema, RegisterBodySchema } from "./schema";
 import { UserSchema } from "../../generated/zod";
-import { hashPassword } from "../../lib/password";
+import { hashPassword, verifyPassword } from "../../lib/password";
 
 export const authRoute = new OpenAPIHono();
 
@@ -28,7 +28,7 @@ authRoute.openapi(
       },
       500: {
         content: { "application/json": { schema: ErrorResponseSchema } },
-        description: "user register failed",
+        description: "User register failed",
       },
     },
   }),
@@ -54,6 +54,71 @@ authRoute.openapi(
         { message: "Failed to registering user", details: error },
         500
       );
+    }
+  }
+);
+
+// ✅ POST /auth/login
+authRoute.openapi(
+  createRoute({
+    tags,
+    summary: "Login user",
+    method: "post",
+    path: "/login",
+    request: {
+      body: {
+        content: { "application/json": { schema: LoginBodySchema } },
+      },
+    },
+    responses: {
+      201: {
+        content: { "application/json": { schema: UserSchema } },
+        description: "User loggd in successfully",
+      },
+      400: {
+        content: { "application/json": { schema: ErrorResponseSchema } },
+        description: "User login failed",
+      },
+      500: {
+        content: { "application/json": { schema: ErrorResponseSchema } },
+        description: "Internal server error",
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const body = c.req.valid("json");
+      const { password, ...userData } = body;
+
+      // const hashedPassword = await hashPassword(password);
+      const user = await prisma.user.findUnique({
+        where: { email: userData.email },
+        include: { password: true },
+      });
+
+      if (!user) {
+        return c.json({ message: "User not found" }, 400);
+      }
+
+      if (!user.password) {
+        return c.json({ message: "User does not have a password" }, 400);
+      }
+
+      const isPasswordValid = await verifyPassword(
+        password,
+        user.password.hash
+      );
+
+      if (!isPasswordValid) {
+        return c.json({ message: "Invalid password" }, 400);
+      }
+
+      // Remove password from the response
+      const { password: _, ...userWithoutPassword } = user;
+
+      return c.json(userWithoutPassword, 201);
+    } catch (error) {
+      return c.json({ message: "User login failed", details: error }, 500);
     }
   }
 );
