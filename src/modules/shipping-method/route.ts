3f41cacb0b74ab1bf4413f233ee83c9792514a99
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-07-04 17:58:21 +0800

    feat: add shipping method route and schema for retrieving all shipping methods

---
+++ src/modules/shipping-method/route.ts
@@ -0,0 +1,33 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import { prisma } from "~/lib/prisma";
+import {
+  ShippingMethodSchema,
+  ShippingMethodsSchema,
+} from "~/modules/shipping-method/schema";
+
+export const shippingMethodRoute = new OpenAPIHono();
+
+const tags = ["Shipping Method"];
+
+// ✅ GET /shipping-methods
+shippingMethodRoute.openapi(
+  createRoute({
+    tags,
+    summary: "Get all shipping methods",
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        content: { "application/json": { schema: ShippingMethodsSchema } },
+        description: "Successfully retrieved all shipping methods",
+      },
+    },
+  }),
+  async (c) => {
+    const shippingMethods = await prisma.shippingMethod.findMany({
+      orderBy: [{ id: "asc" }, { createdAt: "asc" }],
+    });
+
+    return c.json(shippingMethods);
+  }
+);
---
+++ src/modules/shipping-method/schema.ts
@@ -0,0 +1,5 @@
+import { ShippingMethodSchema as BaseShippingMethodSchema } from "~/generated/zod";
+
+export const ShippingMethodSchema = BaseShippingMethodSchema;
+
+export const ShippingMethodsSchema = BaseShippingMethodSchema.array();
