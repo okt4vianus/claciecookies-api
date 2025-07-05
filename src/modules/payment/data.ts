d8f51f7f5ca433fcbf7697ca3a96b6dad30322a7
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-07-05 14:08:12 +0800

    feat: rename 'value' to 'slug' in CreatePayment and CreateShippingMethod types for consistency

--- src/modules/payment/data.ts
+++ src/modules/payment/data.ts
@@ -2,24 +2,24 @@ import { PaymentMethod } from "~/generated/prisma";
 
 export type CreatePayment = Pick<
   PaymentMethod,
-  "name" | "value" | "description"
+  "name" | "slug" | "description"
 >;
 
 // Multiple payment methods for seeding
 export const dataPaymentMethods: CreatePayment[] = [
   {
     name: "Bank Transfer",
-    value: "bank_transfer",
+    slug: "bank_transfer",
     description: "BCA, Mandiri, etc",
   },
   {
     name: "QRIS",
-    value: "qris",
+    slug: "qris",
     description: "Scan static QR code with any supported app",
   },
   {
     name: "E-Wallet",
-    value: "e_wallet",
+    slug: "e_wallet",
     description: "DANA, OVO, ShopeePay, etc",
   },
 ];
--- src/modules/shipping-method/data.ts
+++ src/modules/shipping-method/data.ts
@@ -2,26 +2,26 @@ import { ShippingMethod } from "~/generated/prisma";
 
 export type CreateShippingMethod = Pick<
   ShippingMethod,
-  "name" | "value" | "description" | "price"
+  "name" | "slug" | "description" | "price"
 >;
 
 // Multiple shipping methods for seeding
 export const dataShippingMethods: CreateShippingMethod[] = [
   {
     name: "Same Day (Today)",
-    value: "same_day",
+    slug: "same_day",
     description: "Manado area only",
     price: 50000,
   },
   {
     name: "Express (1-2 business days)",
-    value: "express",
+    slug: "express",
     description: "Fast shipping",
     price: 25000,
   },
   {
     name: "Regular (3-5 business days)",
-    value: "regular",
+    slug: "regular",
     description: "Standard shipping",
     price: 15000,
   },
