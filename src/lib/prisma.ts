44d923960893873c1865e0b424ab65c0fa9416fa
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-07-09 16:49:24 +0800

    feat: add avatar field to User model and update related migrations and scripts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,3 +3,19 @@ import { PrismaClient } from "~/generated/prisma";
 export const prisma = new PrismaClient({
   // log: ["query"],
 });
+
+// Setup Prisma Client with Accelerate
+// const prismaClientSingleton = () => {
+//   return new PrismaClient({}).$extends(withAccelerate());
+// };
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+// // Access NODE_ENV directly without Zod
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
