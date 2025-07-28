1671f031386fa29421fd98862f707408f3a33e78
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-28 16:22:25 +0700

    feat: config better-auth for google signin: GOOGLE_CLIENT_ID GOOGLE_CLIENT_SECRET

--- src/auth.ts
+++ src/auth.ts
@@ -16,10 +16,10 @@ export const auth = betterAuth({
 
   socialProviders: {
     // TODO: Enable later
-    // google: {
-    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    // },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
     // facebook: {
     //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
     //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
